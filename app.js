b1ef457b7386dc5eed4119b20796674eb9f8e2c2
Mael <gonzalezremael@gmail.com>
2024-02-13 10:34:50 -0600

    Se resuelven los puntos 2 y 3

--- app.js
+++ app.js
@@ -12,3 +12,14 @@ function funcionCambio(item, value) {
 }
 
 funcionCambio("h1","Hora del Desafío");
+
+// 2.- Crea una función que muestre en la consola el mensaje "El botón fue clicado" siempre que se presione el botón "Console".
+function btnClick() {
+    console.log("El botón fue clicado");
+}
+
+// 3.- Crea una función que se ejecute cuando se haga clic en el botón "prompt", preguntando el nombre de una ciudad de Brasil. Luego, muestra una alerta con el mensaje concatenando la respuesta con el texto: "Estuve en {ciudad} y me acordé de ti".
+function promptBtn() {
+    let ciudad = prompt("Ingresa el nombre de una ciudad:");
+    alert(`Estuve en ${ciudad} y me acordé de ti`);
+}
